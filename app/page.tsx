386d15bc9ac16e3907b2a6ff1a584a23435d4942
Diky Arif Febriyanto <dikyariff97@gmail.com>
2024-02-09 15:20:21 +0700

    chore: add authentication with clerk and convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <Button size={"lg"} variant={"destructive"}>
-      Click Me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <div>
+        This is a screen for authenticated user only
+      </div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
   );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://relevant-lizard-89.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
