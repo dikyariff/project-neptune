0a3088c4580905c460125cbd30cf87abde48fe79
Diky Arif Febriyanto <dikyariff97@gmail.com>
2024-02-09 22:04:56 +0700

    chore: create empty state

---
+++ app/(dashboard)/_components/empty-org.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { CreateOrganization } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+
+export const EmptyOrg = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image src="/no-boards.svg" alt="empty" height={200} width={200} />
+      <h2 className="text-2xl font-semibold mt-6">Welcome to Neptune</h2>
+      <p className="text-muted-foreground text-sm mt-2">
+        Create an{" "}
+        <span className="font-semibold text-blue-500">Organization</span> to get
+        started
+      </p>
+      <div className="mt-6">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="lg">Create Organization</Button>
+          </DialogTrigger>
+          <DialogContent className="w-fit p-0 bg-transparent border-none max-w-[480px]">
+            <CreateOrganization />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
